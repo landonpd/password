69a92fd8f5e3df34bb4ee96b264e98fde4b2be03
landonpd <landonpd80@gmail.com>
2024-08-10 15:36:36 -0600

    made a way to change the master password

--- userInterface/userInterface.go
+++ userInterface/userInterface.go
@@ -346,6 +346,9 @@ func updatePasswords(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 			strToCopy := m.passwords[m.cursor].EncryptedPswrd
 			clipboard.Write(clipboard.FmtText, []byte(strToCopy))
 			//m.cursor=0
+		case "m", "M":
+			//code to copy here
+			m.display = NewAcctDisplay
 		case "q", "Q", "ctrl+c", "ctrl+z", "esc":
 			// stor.WritePasswordsCharm(m.passwords, len(m.passwords[0].Website), m.db, m.config)
 			return exitTasks(m)
@@ -358,7 +361,7 @@ func updatePasswords(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 // u/i screen to display the passwords, default screen display
 func passwordsDisplayView(m model) string {
 	var s string
-	s += "\nPress c(copy), a(add), d(delete), u(update) or q(quit)\n\n"
+	s += "\nPress c(copy), a(add), d(delete), u(update),  m(change master) or q(quit)\n\n"
 	for i, savedPasswords := range m.passwords {
 		cursor := " " // no cursor
 		if m.cursor == i {
